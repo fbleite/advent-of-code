7fe6be608d6629935e823347d8b3f75d37a582c3
fbleite <feliperbleite@gmail.com>
2024-12-10 10:10:35 -0500

    Day 10

---
+++ 2024/src/main/java/Day10.java
@@ -0,0 +1,69 @@
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Day10 {
+
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        String input = FileUtil.getFileString("Day10.txt");
+        var map = parseInput(input);
+        System.out.println(calculateMapScore(map, false));
+        System.out.println(calculateMapScore(map, true));
+    }
+
+    public static int[][] parseInput(String input) {
+        var lines = input.split("\n|\r\n");
+        var result = new int[lines.length][lines[0].length()];
+        for (int i = 0; i < lines.length; i++) {
+            for (int j = 0; j < lines[0].length(); j++) {
+                result[i][j] = lines[i].charAt(j) - '0';
+            }
+        }
+        return result;
+    }
+
+    public record Coordinate(int i, int j) {
+    }
+
+    public static int calculateMapScore(int[][] map, boolean multiplePath) {
+        int sum = 0;
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                if (map[i][j] == 0) {
+                    Map<Coordinate, Integer> tops = new HashMap<>();
+                    getTrailheadTops(i, j, map, tops);
+                    if (multiplePath) {
+                        for (var key : tops.keySet()) {
+                            sum += tops.get(key);
+                        }
+                    } else {
+                        sum += tops.size();
+                    }
+                }
+            }
+        }
+        return sum;
+    }
+
+    public static void getTrailheadTops(int i, int j, int[][] map, Map<Coordinate, Integer> tops) {
+        if (map[i][j] == 9) {
+            tops.put(new Coordinate(i, j), tops.getOrDefault(new Coordinate(i, j), 0) + 1);
+        }
+        if (i - 1 >= 0 && map[i - 1][j] == map[i][j] + 1) {
+            getTrailheadTops(i - 1, j, map, tops);
+        }
+
+        if (j - 1 >= 0 && map[i][j - 1] == map[i][j] + 1) {
+            getTrailheadTops(i, j - 1, map, tops);
+        }
+
+        if (i + 1 < map.length && map[i + 1][j] == map[i][j] + 1) {
+            getTrailheadTops(i + 1, j, map, tops);
+        }
+
+        if (j + 1 < map[0].length && map[i][j + 1] == map[i][j] + 1) {
+            getTrailheadTops(i, j + 1, map, tops);
+        }
+    }
+}
---
+++ 2024/src/test/java/Day10Test.java
@@ -0,0 +1,93 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day10Test {
+
+    @Test
+    void parseInput() {
+        var input = """
+                0123
+                1234
+                8765
+                9876
+                """;
+        var actual = Day10.parseInput(input);
+        var expected = new int [][] {
+                {0,1,2,3},
+                {1,2,3,4},
+                {8,7,6,5},
+                {9,8,7,6}
+        };
+        assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    void getTrailheadTops() {
+        var input = """
+                0123
+                1234
+                8765
+                9876
+                """;
+        var map = Day10.parseInput(input);
+        Map<Day10.Coordinate, Integer> tops = new HashMap<>();
+        Day10.getTrailheadTops(0, 0, map, tops);
+        assertEquals(1, tops.size());
+    }
+
+    @Test
+    void getTrailheadTops_multiple() {
+        var input = """
+1110111
+1111111
+1112111
+6543456
+7111117
+8111118
+9111119
+                """;
+        var map = Day10.parseInput(input);
+        Map<Day10.Coordinate, Integer> tops = new HashMap<>();
+        Day10.getTrailheadTops(0, 3, map, tops);
+        assertEquals(2, tops.size());
+    }
+
+    @Test
+    void calculateMapScore() {
+        var input = """
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
+                """;
+        var map = Day10.parseInput(input);
+        assertEquals(36, Day10.calculateMapScore(map, false));
+    }
+
+    @Test
+    void calculateMapScore_multiplePath() {
+        var input = """
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
+                """;
+        var map = Day10.parseInput(input);
+        assertEquals(81, Day10.calculateMapScore(map, true));
+    }
+}
